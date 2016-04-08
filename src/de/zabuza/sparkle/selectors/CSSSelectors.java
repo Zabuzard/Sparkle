ee84d76afe8aad40020eadface74762d366521cb
ZabuzaW <Slippy-Toad@web.de>
2016-04-08 02:13:53 +0200

    Refactoring

--- src/de/zabuza/sparkle/selectors/CSSSelectors.java
+++ src/de/zabuza/sparkle/selectors/CSSSelectors.java
@@ -5,10 +5,8 @@ public final class CSSSelectors {
 	public static final String ITEM_INVENTORY_CLOSE_ANCHOR = "p.listcaption a[href$=closeinv]";
 	public static final String ITEM_INVENTORY_ITEM_NAME = "p.listitemrow b";
 	public static final String ITEM_INVENTORY_OPEN_ANCHOR = "p.listcaption a[href$=openinv]";
-
 	public static final String ITEM_PLAYER_ATTACK_POINTS = "p#listrow_attackp";
 	public static final String ITEM_PLAYER_ATTACK_WEAPON = "p#listrow_attackw";
-
 	public static final String ITEM_PLAYER_DEFENSE_POINTS = "p#listrow_defensep";
 	public static final String ITEM_PLAYER_DEFENSE_WEAPON = "p#listrow_defensew";
 	public static final String ITEM_PLAYER_GOLD = "p#listrow_money";
@@ -18,6 +16,7 @@ public final class CSSSelectors {
 	public static final String ITEM_PLAYER_NAME_EXPERIENCE = "div#toplpheader + p.listcaption";
 	public static final String ITEM_PLAYER_SPEED = "p#listrow_int span";
 	public static final String ITEM_PLAYER_STATUS = "p#listrow_status";
+	
 	public static final String LOGIN_FORM_NAME = ".logininput[name=name]";
 	public static final String LOGIN_FORM_PASSWORD = ".logininput[name=password]";
 	public static final String LOGIN_FORM_SUBMIT = ".loginsubmit[name=submit]";
@@ -27,9 +26,10 @@ public final class CSSSelectors {
 	public static final String MAIN_LOCATION_NPC_ATTACK_REGULAR_ANCHOR = "p.maindesc2 a[href*='attacknpc']";
 	public static final String MAIN_LOCATION_NPC_ATTACK_SINGLE_ANCHOR = "p.maindesc2 a[href*='slapnpc']";
 	public static final String MAIN_LOCATION_NPC_NAME = "p.listusersrow b";
+	
 	public static final String MAP_FRAME_PRESENT = "p.positiontext";
-	private static final String MAP_MOVE_ANCHOR_POST = "\\')']";
-	private static final String MAP_MOVE_ANCHOR_PRE = "a[onclick*='Move(\\'";
+	public static final String MAP_MOVE_ANCHOR_POST = "\\')']";
+	public static final String MAP_MOVE_ANCHOR_PRE = "a[onclick*='Move(\\'";
 	public static final String MAP_MOVE_EAST_ANCHOR = MAP_MOVE_ANCHOR_PRE + "right" + MAP_MOVE_ANCHOR_POST;
 	public static final String MAP_MOVE_NORTH_ANCHOR = MAP_MOVE_ANCHOR_PRE + "up" + MAP_MOVE_ANCHOR_POST;
 	public static final String MAP_MOVE_NORTHEAST_ANCHOR = MAP_MOVE_ANCHOR_PRE + "upright" + MAP_MOVE_ANCHOR_POST;
@@ -40,7 +40,7 @@ public final class CSSSelectors {
 	public static final String MAP_MOVE_WEST_ANCHOR = MAP_MOVE_ANCHOR_PRE + "left" + MAP_MOVE_ANCHOR_POST;
 	public static final String MAP_POSITION_TEXT = "p.positiontext";
 	public static final String MAP_SURELY_LOGOUT_ANCHOR = "a[href^=logout]";
-
+	
 	public static final String MENU_FRAME_PRESENT = "a[href^=logout]";
 	public static final String MENU_LOGOUT_ANCHOR = "a[href^=logout]";
 
